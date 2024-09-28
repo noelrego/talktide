e25b65180113b3ed79a2e95eb9b348468a368587
noelrego <noelrego.nnr@gmail.com>
2024-09-29 04:41:56 +0530

    UX design done

--- frontend/app/src/app/chat/chat-box/chat-box.component.ts
+++ frontend/app/src/app/chat/chat-box/chat-box.component.ts
@@ -9,60 +9,98 @@ export class ChatBoxComponent {
 
   userName: string = 'User\'s Name';
   newMessage: string = '';
+  showPreviewMessage: boolean = false;
+  replayingToPreviewMsg: string = ''
 
   // Example messages for demonstration
   messages = [
     {
-      text: 'Hello! How are you doing today?',
-      profileImage: 'https://via.placeholder.com/48x48',
+      msgId: 1,
+      text: 'First Msg asiudaisu aisdiuasdu iausdiausgdiuasdas auisdiuagsdiasudliaa asiduagisudgasiudg',
+      hasPreview: false,
+      replayedBy: 'Noel',
+      replayedContent: 'ok',
+      replayedToMessageId: 10,
       time: '10:01 AM',
       side: 'left'
     },
     {
+      msgId: 2,
       text: 'I\'m good, thank you! How about you?',
-      profileImage: 'https://via.placeholder.com/48x48',
+      hasPreview: false,
+      replayedBy: 'Noel',
+      replayedContent: 'ok',
+      replayedToMessageId: 10,
       time: '10:02 AM',
       side: 'right'
     },
     {
+      msgId: 3,
       text: 'I\'m doing well too, thanks for asking.',
-      profileImage: 'https://via.placeholder.com/48x48',
+      hasPreview: true,
+      replayedBy: 'Noel',
+      replayedContent: 'ok',
+      replayedToMessageId: 10,
       time: '10:03 AM',
       side: 'left'
     },
     {
+      msgId: 4,
       text: 'Hello! How are you doing today?',
-      profileImage: 'https://via.placeholder.com/48x48',
+      hasPreview: false,
+      replayedBy: 'Noel',
+      replayedContent: 'ok',
+      replayedToMessageId: 10,
       time: '10:01 AM',
       side: 'left'
     },
     {
+      msgId: 5,
       text: 'I\'m good, thank you! How about you?',
-      profileImage: 'https://via.placeholder.com/48x48',
+      hasPreview: false,
+      replayedBy: 'Noel',
+      replayedContent: 'ok',
+      replayedToMessageId: 10,
       time: '10:02 AM',
       side: 'right'
     },
     {
+      msgId: 6,
       text: 'I\'m doing well too, thanks for asking.',
-      profileImage: 'https://via.placeholder.com/48x48',
+      hasPreview: true,
+      replayedBy: 'Noel',
+      replayedContent: 'ok',
+      replayedToMessageId: 10,
       time: '10:03 AM',
       side: 'left'
     },
     {
+      msgId: 17,
       text: 'Hello! How are you doing today?',
-      profileImage: 'https://via.placeholder.com/48x48',
+      hasPreview: false,
+      replayedBy: 'Noel',
+      replayedContent: 'ok',
+      replayedToMessageId: 10,
       time: '10:01 AM',
       side: 'left'
     },
     {
-      text: 'I\'m good, thank you! How about you?',
-      profileImage: 'https://via.placeholder.com/48x48',
+      msgId: 8,
+      text: 'I\'m good, thank you! How about you? asiauhsiuahsiuhdaisuhdiuhahsu',
+      hasPreview: true,
+      replayedBy: 'Noel',
+      replayedContent: 'Blah Blah',
+      replayedToMessageId: 10,
       time: '10:02 AM',
       side: 'right'
     },
     {
-      text: 'I\'m doing well too, thanks for asking.',
-      profileImage: 'https://via.placeholder.com/48x48',
+      msgId: 9,
+      text: 'Last Msg',
+      hasPreview: true,
+      replayedBy: 'Noel',
+      replayedContent: 'ok',
+      replayedToMessageId: 10,
       time: '10:03 AM',
       side: 'left'
     }
@@ -71,13 +109,28 @@ export class ChatBoxComponent {
   sendMessage() {
     if (this.newMessage.trim()) {
       this.messages.push({
+        msgId: 1,
         text: this.newMessage,
-        profileImage: 'https://via.placeholder.com/48x48', // Placeholder, replace with actual user's image
+        hasPreview: false,
+        replayedBy: 'Noel',
+        replayedContent: 'Some msg',
+        replayedToMessageId: 13,
         time: new Date().toLocaleTimeString(),
         side: 'right'
       });
       this.newMessage = ''; // Clear input
     }
+  }
+
+  clickReplayMessage(msgId: number, msg: string) {
+    console.log('Clicked Replay', msgId, msg);
+    this.showPreviewMessage = true;
+    this.replayingToPreviewMsg = msg.slice(0, 40) + ' . . .';
+  }
+
+  closePreview() {
+    this.showPreviewMessage = false;
+    this.replayingToPreviewMsg = '';
   }
 
 }
